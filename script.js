36557982352a8b31c0c82fc951212466f42aa21b
Zeenyk <federico05pese@gmail.com>
2025-05-23 15:14:54 +0200

    Trying vanishing header

--- script.js
+++ script.js
@@ -112,4 +112,22 @@ sandbox.load(`
 `);
 
 
+let lastScrollY = window.scrollY;
+const sections = document.querySelectorAll('.my-section');
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  sections.forEach((section) => {
+    if (currentScrollY > lastScrollY) {
+      section.style.position = 'relative';
+    } else if (currentScrollY < lastScrollY) {
+      section.style.position = 'absolute';
+    }
+  });
+
+  lastScrollY = currentScrollY;
+});
+
+
 
